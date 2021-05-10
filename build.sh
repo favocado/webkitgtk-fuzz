fe0f9842b1b4ba591d0792ccd087825aafb13d88
mipu94 <tadinhsung@gmail.com>
2021-05-10 14:27:58 +0700

    remove unuse code

--- build.sh
+++ build.sh
@@ -10,7 +10,6 @@ FUZZ_TYPE="context-dependent"
 # FUZZ_TYPE="non-context"
 SCRIPT=`realpath $0`
 SCRIPTPATH=`dirname $SCRIPT`
-CORPUS="$SCRIPTPATH/corpus"
 mkdir -p crashes/webkit${WEBKIT_VERSION}
 mkdir corpus
 
@@ -19,17 +18,3 @@ docker build . -t webkit_asan${WEBKIT_VERSION} --build-arg WEBKIT_VERSION=${WEBK
 for ((i = 0 ; i <= 2 ; i++)); do
 	docker run -it --cpus="2.0" --memory="2g"  -d -v $SCRIPTPATH/crashes/webkit${WEBKIT_VERSION}:/root/crashes -v $SCRIPTPATH/corpus:/root/corpus  --name="fuzzwebkit$i" webkit_asan${WEBKIT_VERSION} /bin/bash -c /resource/run.sh
 done
-
-if [ $FUZZ_TYPE == "context-free" ]
-	then
-		while true
-		do
-			if [ $(ls ./corpus| wc -l) -le 50 ]
-				then
-					find $CORPUS -name '*.html' | head -n 50 | xargs -I {} mv {} corpus;
-					echo " moved 50 new files";
-					sleep 20;
-			fi
-			sleep 20;
-		done
-fi
